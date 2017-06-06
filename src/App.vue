f5ff9d7e5fdbc3dda0b06b7b30b42b0e4d077d7c
DomenLee <1195612571@qq.com>
2017-06-06 11:11:45 +0800

    domen
    首次提交

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+<div id="app">
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted () {
+    // 自适应字体
+
+    // 设置viewport
+    // const viewport = document.querySelector(`meta[name=viewport]`)
+    // const devicePixelRatio = +window.devicePixelRatio
+    // let value
+    //
+    // // 判断
+    // switch (devicePixelRatio) {
+    //   case 1:
+    //     value = 1
+    //     break
+    //
+    //   case 2:
+    //     value = 0.5
+    //     break
+    //
+    //   case 3:
+    //     value = 0.3333333333333333
+    //     break
+    // }
+    //
+    // viewport.setAttribute(`content`, `width=device-width, initial-scale=` +
+    //   value + `, maximum-scale=` +
+    //   value + `, minimum-scale=` +
+    //   value + `, user-scalable=no`)
+
+    let setRootFontSize = () => {
+      let html = document.documentElement
+      let fontSize = html.clientWidth * 2 / 7.5
+      html.style.fontSize = fontSize + `px`
+    }
+
+    setRootFontSize()
+
+    // 测试时使用，上线可删除，非自适应(只支持手机端)
+    window.addEventListener(`resize`, () => {
+      setTimeout(() => {
+        setRootFontSize()
+      }, 1000)
+    }, false)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Aveni', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+
+body, html {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  background-color: #e3e3e3;
+  font-size: .1rem;
+}
+
+.clear:after {
+  content: '';
+  display: block;
+  clear: both;
+}
+
+.fl {
+  float: left;
+}
+
+.fr {
+  float: right;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <mt-header fixed
+      :title="headerTitle"
+    ></mt-header>
+    <section class="firm-info clear">
+      <img class="fl firm-logo" src="../assets/logo.png">
+      <div class="content fl">
+        美来登(中国)有限公司
+      </div>
+      <div class="firm-info-star fr">
+        <div class="fr">
+          <mt-button size="small" type="danger" @click="attentioned">
+            {{ stared ? '取关' : '关注'}}
+          </mt-button>
+        </div>
+        <span class="fr">粉丝数：{{ stars }}</span>
+      </div>
+    </section>
+    <mt-navbar v-model="selected">
+      <mt-tab-item id="1">选项一</mt-tab-item>
+      <mt-tab-item id="2">选项二</mt-tab-item>
+      <mt-tab-item id="3">选项三</mt-tab-item>
+    </mt-navbar>
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="1">
+        <mt-cell title="企业地址" value="广东省广州市天河区城中大道皇家花园G6栋58..."></mt-cell>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        <mt-cell v-for="n in 4" :title="'测试 ' + n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="3">
+        <mt-cell v-for="n in 6" :title="'选项 ' + n" />
+      </mt-tab-container-item>
+    </mt-tab-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      headerTitle: '厂商主页',
+      stars: 100,
+      stared: true,
+      selected: '1'
+    }
+  },
+  methods: {
+    attentioned () {
+      this.stars = this.stared ? this.stars - 1 : this.stars + 1
+      this.stared = !this.stared
+    }
+  }
+}
+</script>
+
+<style>
+  .firm-info {
+    background: #fff;
+    margin: .1rem;
+    padding: .1rem;
+    /*overflow: hidden;*/
+  }
+
+  .mint-cell-title {
+    font-size: .12rem;
+  }
+
+  .mint-cell-value {
+    font-size: .1rem;
+  }
+
+ .firm-logo {
+   width: .6rem;
+   height: .6rem;
+   border: .04rem solid #ececec;
+   border-radius: .6rem;
+ }
+
+ .firm-info .content {
+   width: 1.6rem;
+   text-align: center;
+   font-size: .1rem;
+   line-height: .68rem;
+ }
+
+ .firm-info-star {
+   width: 1rem;
+   line-height: 1;
+   font-size: .1rem;
+ }
+
+ .firm-info-star span {
+   margin-top: .1rem;
+   display: inline-block;
+ }
+ .mint-cell-value {
+   font-size: .1rem;
+ }
+</style>
